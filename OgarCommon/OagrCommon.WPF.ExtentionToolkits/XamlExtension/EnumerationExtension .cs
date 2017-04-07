40204b583a3c88c859f36bf069ba1b7b5d37011a
zp <sitandevil@gmail.com>
2017-04-07 15:51:55 +0800

    添加wpf的拓展工具库，新增xaml枚举拓展标记，可将枚举类型转换为列表

---
+++ OgarCommon/OagrCommon.WPF.ExtentionToolkits/XamlExtension/EnumerationExtension .cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Globalization;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Markup;
+
+namespace OagrCommon.WPF.ExtentionToolkits
+{
+    public class EnumerationExtension : MarkupExtension
+    {
+        private Type _enumType;
+        private IEnumerable<EnumerationMember> members;
+
+        public class EnumerationMember
+        {
+            public string Description { get; set; }
+            public object Value { get; set; }
+        }
+
+        public Type EnumType
+        {
+            get { return _enumType; }
+            private set
+            {
+                if (_enumType == value)
+                    return;
+
+                var enumType = Nullable.GetUnderlyingType(value) ?? value;
+
+                if (enumType.IsEnum == false)
+                    throw new ArgumentException("Type must be an Enum.");
+
+                _enumType = value;
+            }
+        }
+
+        public EnumerationExtension(Type enumType)
+        {
+            if (enumType == null)
+                throw new ArgumentNullException("enumType");
+
+            EnumType = enumType;
+        }
+
+        public override object ProvideValue(IServiceProvider serviceProvider)
+        {
+            members = GetAllValuesAndDescriptions(EnumType);
+            return members;
+        }
+
+        public string GetDescription(Enum value)
+        {
+            Type enumType = value.GetType();
+            // 获取枚举常数名称。
+            string name = Enum.GetName(enumType, value);
+            if (name != null)
+            {
+                // 获取枚举字段。
+                FieldInfo fieldInfo = enumType.GetField(name);
+                if (fieldInfo != null)
+                {
+                    // 获取描述的属性。
+                    DescriptionAttribute attr = Attribute.GetCustomAttribute(fieldInfo,
+                        typeof(DescriptionAttribute), false) as DescriptionAttribute;
+                    if (attr != null)
+                    {
+                        return attr.Description;
+                    }
+                }
+            }
+            return null;
+        }
+
+        public IEnumerable<EnumerationMember> GetAllValuesAndDescriptions(Type t)
+        {
+            if (!t.IsEnum)
+                throw new ArgumentException("t must be an enum type");
+
+            var enumValues = Enum.GetValues(t).Cast<Enum>();
+
+            return (
+              from Enum enumValue in enumValues
+              select new EnumerationMember
+              {
+                  Value = enumValue,
+                  Description = GetDescription(enumValue)
+              }).ToList();
+        }
+    }
+}
