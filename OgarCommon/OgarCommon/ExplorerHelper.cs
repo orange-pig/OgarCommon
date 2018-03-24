901acf57f4fbf3031f9800cf36fcb4e74cd930aa
zhoupeng <sitandevil@gmail.com>
2018-03-24 17:32:38 +0800

    add browse file in explorer function

---
+++ OgarCommon/OgarCommon/ExplorerHelper.cs
@@ -0,0 +1,52 @@
+﻿using OgarCommon.Win32Api;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OgarCommon
+{
+    public static class ExplorerHelper
+    {
+        /// <summary>
+        /// Open file use exporer.
+        /// </summary>
+        /// <param name="filePath">File full path be opened</param>
+        /// <param name="selected">Select file</param>
+        /// <param name="useExisted">If existed explorer use it, not new one.</param>
+        public static void BrowseFile(string filePath, bool selected = false, bool useExisted = true)
+        {
+            if (filePath == null)
+                throw new ArgumentNullException("filePath");
+
+            if (useExisted)
+            {
+                if (selected)
+                {
+                    IntPtr pidl = Shell32.ILCreateFromPathW(filePath);
+                    Shell32.SHOpenFolderAndSelectItems(pidl, 0, IntPtr.Zero, 0);
+                    Shell32.ILFree(pidl);
+                }
+                else
+                {
+                    var path = Path.GetDirectoryName(filePath);
+                    Process.Start(path);
+                }
+            }
+            else
+            {
+                if (selected)
+                {
+                    Process.Start("Explorer.exe", string.Format("/select,{0}", filePath));
+                }
+                else
+                {
+                    Process.Start("Explorer.exe", Path.GetFullPath(filePath));
+                }
+            }
+        }
+    }
+}
---
+++ OgarCommon/OgarCommon/Win32Api/Shell32.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OgarCommon.Win32Api
+{
+    public class Shell32
+    {
+        [DllImport("shell32.dll", CharSet = CharSet.Unicode)]
+        public static extern IntPtr ILCreateFromPathW(string pszPath);
+
+        [DllImport("shell32.dll")]
+        public static extern int SHOpenFolderAndSelectItems(IntPtr pidlFolder, int cild, IntPtr apidl, int dwFlags);
+
+        [DllImport("shell32.dll")]
+        public static extern void ILFree(IntPtr pidl);
+    }
+}
