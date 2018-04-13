fe63677428f22a44a957ad6364fe62420f0b1512
zhoupeng <sitandevil@gmail.com>
2018-04-13 19:39:57 +0800

    add FloatToDoubleConverter descrption

--- OgarCommon/OgarCommon.WPF.LokTarMarkup/Converters/FloatToDoubleConverter.cs
+++ OgarCommon/OgarCommon.WPF.LokTarMarkup/Converters/FloatToDoubleConverter.cs
@@ -8,6 +8,9 @@ using System.Windows.Data;
 
 namespace OgarCommon.WPF.LokTarMarkup
 {
+    /// <summary>
+    /// Float converter to double easy to loses precision
+    /// </summary>
     public class FloatToDoubleConverter : IValueConverter
     {
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
