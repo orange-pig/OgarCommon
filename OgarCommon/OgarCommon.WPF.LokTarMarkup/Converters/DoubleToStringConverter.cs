e36f3c0153b9d6b02b3da5651e6f49f7c86a46a8
zhoupeng <sitandevil@gmail.com>
2018-04-13 19:41:12 +0800

    add DoubleToStringConverter

---
+++ OgarCommon/OgarCommon.WPF.LokTarMarkup/Converters/DoubleToStringConverter.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace OgarCommon.WPF.LokTarMarkup
+{
+    /// <summary>
+    /// Double to String , or String to Double
+    /// </summary>
+    public class DoubleToStringConverter : IValueConverter
+    {
+        private bool _isReversed;
+
+        public bool IsReversed { get { return _isReversed; } set { _isReversed = value; } }
+
+        public DoubleToStringConverter()
+            : this(false)
+        {
+
+        }
+
+        public DoubleToStringConverter(bool isReversed)
+        {
+            _isReversed = isReversed;
+        }
+
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return !IsReversed ? ConvertToString(value) : ConvertToDouble(value);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return !IsReversed ? ConvertToDouble(value) : ConvertToString(value);
+        }
+
+
+        private object ConvertToDouble(object value)
+        {
+            return double.TryParse(value.ToString(), out double number) ? number : Binding.DoNothing;
+        }
+
+        private object ConvertToString(object value)
+        {
+            return value == null ? null : value.ToString();
+        }
+    }
+}
---
+++ OgarCommon/OgarCommon.WPF.LokTarMarkup/Markup/DoubleToStringConverterExtension.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Markup;
+
+namespace OgarCommon.WPF.LokTarMarkup
+{
+    public class DoubleToStringConverterExtension : MarkupExtension
+    {
+        private bool _isReversed;
+
+        [ConstructorArgument("isReversed")]
+        public bool IsReversed
+        {
+            get { return _isReversed; }o
+            set { _isReversed = value; }
+        }
+
+        public override object ProvideValue(IServiceProvider serviceProvider)
+        {
+            return new DoubleToStringConverter(_isReversed);
+        }
+    }
+}
