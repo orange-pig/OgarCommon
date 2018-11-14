7002febfb03fbda4d001b79343839634b4860504
zhoupeng <sitandevil@gmail.com>
2018-11-14 17:26:25 +0800

    remove log帮助库

--- OgarCommon/OgarCommon/ThreadHelper.cs
+++ OgarCommon/OgarCommon/ThreadHelper.cs
@@ -18,26 +18,7 @@ namespace OgarCommon
         /// <param name="target"></param>
         public static void ForwardActionToThreadPool(object eventData, Action<object> target)
         {
-            try
-            {
-                ThreadPool.QueueUserWorkItem(new WaitCallback(target), eventData);
-            }
-            catch (ApplicationException ex)
-            {
-                LogManager.Error("FaceWrap添加线程时应用程序错误," + ex.Message + "," + ex.ToString());
-            }
-            catch (OutOfMemoryException ex)
-            {
-                LogManager.Error("FaceWrap添加线程时内存不足," + ex.Message + "," + ex.ToString());
-            }
-            catch (ArgumentNullException ex)
-            {
-                LogManager.Error("FaceWrap添加线程时参数为空," + ex.Message + "," + ex.ToString());
-            }
-            catch (Exception ex)
-            {
-                LogManager.Error("FaceWrap添加线程时异常," + ex.Message + "," + ex.ToString());
-            }
+            ThreadPool.QueueUserWorkItem(new WaitCallback(target), eventData);
         }
     }
 }
